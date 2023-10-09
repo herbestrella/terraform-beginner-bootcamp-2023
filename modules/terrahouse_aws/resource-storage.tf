433d49ba292e7dee1d1d8a4e5355357b9ae28867
herb estrella <herbert.estrella@gmail.com>
2023-10-09 18:37:14 +0000

    #36 only upload index.html if a content version has changed

--- modules/terrahouse_aws/resource-storage.tf
+++ modules/terrahouse_aws/resource-storage.tf
@@ -26,6 +26,10 @@ resource "aws_s3_object" "index_html" {
   content_type = "text/html"
 
   etag = filemd5(var.index_html_filepath)
+  lifecycle {
+    replace_triggered_by = [terraform_data.content_version.output]
+    ignore_changes = [etag]
+  }
 }
 #https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_object
 resource "aws_s3_object" "error_html" {
@@ -35,6 +39,9 @@ resource "aws_s3_object" "error_html" {
   content_type = "text/html"
   
   etag = filemd5(var.error_html_filepath)
+  #lifecycle {
+    #ignore_changes = [etag]
+  #}
 }
 #https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_policy
 resource "aws_s3_bucket_policy" "bucket_policy" {
@@ -59,3 +66,7 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
     },
   })
 } 
+
+resource "terraform_data" "content_version" {
+  input = var.content_version
+}
